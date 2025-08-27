c2ca54a4e3449a59cf18e6659d8ee0ef9919eede
laurabpaulsen <202005791@post.au.dk>
2025-08-27 11:27:02 +0200

    change port address

--- utils/triggers.py
+++ utils/triggers.py
@@ -9,7 +9,7 @@ PLATFORM = platform.platform()
 if 'Linux' in PLATFORM:
     port = parallel.ParallelPort(address='/dev/parport0')  # on MEG stim PC
 else:  # on Win this will work, on Mac we catch error below
-    port = parallel.ParallelPort(address=0xDFF8)  # on MEG stim PC
+    port = parallel.ParallelPort(address="LPT3") 
 
 # NB problems getting parallel port working under conda env
 # from psychopy.parallel._inpout32 import PParallelInpOut32
