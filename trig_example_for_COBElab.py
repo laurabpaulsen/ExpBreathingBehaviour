4082770ef07257fb86b7543a995ce1bf57ecc82a
laurabpaulsen <202005791@post.au.dk>
2025-08-29 11:17:42 +0200

    trig example for cobelab

---
+++ trig_example_for_COBElab.py
@@ -0,0 +1,31 @@
+from utils.triggers import create_trigger_mapping
+
+
+from psychopy import parallel
+from psychopy.clock import CountdownTimer
+
+
+port = parallel.ParallelPort(address=0x3FD8)
+trigger_mapping = create_trigger_mapping()
+countdown_timer = CountdownTimer()
+
+
+def setParallelData(code=1):
+    port.setData(code)
+    
+
+def raise_and_lower_trigger(trigger, duration=0.001):
+    setParallelData(trigger)
+
+    countdown_timer.reset(duration)
+    while countdown_timer.getTime() > 0:
+        pass
+    setParallelData(0)
+
+
+for i in range(1, 6):
+    for description, trig_val in trigger_mapping.items():
+        countdown_timer.reset(0.5)
+        while not countdown_timer.getTime() > 0:
+            pass
+        raise_and_lower_trigger(trigger=trig_val)
