5e93ebd92a24880b5d354435367d003646ca3b5f
laurabpaulsen <202005791@post.au.dk>
2025-08-29 11:39:34 +0200

    remove irrelevant comments

--- utils/triggers.py
+++ utils/triggers.py
@@ -3,16 +3,10 @@ Description: This file contains the code for sending triggers to the neuroimagin
 """
 # -*- coding: utf-8 -*-
 from psychopy import parallel
-import platform
+
 
 port = parallel.ParallelPort(address=0x3FD8)
 print(f"Parallel port {port} initialised.")
-
-# NB problems getting parallel port working under conda env
-# from psychopy.parallel._inpout32 import PParallelInpOut32
-# port = PParallelInpOut32(address=0xDFF8)  # on MEG stim PC
-# parallel.setPortAddress(address='0xDFF8')
-# port = parallel
 
 # Figure out whether to flip pins or fake it
 try:
